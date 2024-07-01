8835393d3c6f5fe53fcd4ac63e909b3c2ec98f5e
Adeeb <kayyumahmad07860@gmail.com>
2024-07-01 17:33:25 +0530

    security changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,5 +2,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async() => {
-    await mongoose.connect('mongodb+srv://AdeebShekh:95804adeebsh@cluster0.zsp1s76.mongodb.net/food-del').then(()=>console.log("DB connected"));
+    await mongoose.connect('idhar mat dekho').then(()=>console.log("DB connected"));
 }
